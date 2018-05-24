1372ddda44e93b37f6ad2a2fe6e763e8bab56aa8
Nontawat <59161165@go.buu.ac.th>
2018-05-25 02:12:34 +0700

    get

--- server.js
+++ server.js
@@ -3,17 +3,29 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = []
+let contacts = [
+    {
+        name : 'John',
+        phoneNumber : '011-1111111'
+    }
+]
 
 app.use(bodyParser.json())
 app.use(cors())
 
 /// TODO: Develop GET /contacts
+app.get('/contacts',(req,res) =>{
+    res.json(contacts)
+})
 
 
 
 /// TODO: Develop POST /contacts
-
+app.post('/contacts', (req,res) => {
+    let newContacts = req.body
+    contacts.push(newContacts)
+    res.status(201).send()
+})
 
 
 app.listen(3000, () => {
